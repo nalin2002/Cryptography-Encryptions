9e8a7854fadb5d6da0eeedaca693adbccd2b81ff
Nalin Prabhath <48054155+nalin2002@users.noreply.github.com>
2021-02-08 10:29:59 +0530

    Basic Encryptions

---
+++ ASymm_Encryption.py
@@ -0,0 +1,43 @@
+#-----------------Public Key Encryption--------------
+
+# RSA Cryptography
+# Ref: https://medium.com/@ashiqgiga07/asymmetric-cryptography-with-python-5eed86772731
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from binascii import hexlify
+
+file_name='Test.txt'
+with open(file_name,'w') as f:
+    f.write("Public and Private key encryption")
+with open(file_name,'rb') as f:
+    mssg=f.read()
+
+print(mssg)
+# generating private key of length of 1024 bits
+private_key=RSA.generate(1024)
+# generating the public key from private key
+public_key=private_key.publickey()
+
+# private_key and public_key are RSA Key objects .Now converting them to strings
+private_key_str= private_key.export_key().decode()
+public_key_str= public_key.export_key().decode()
+
+#writing the public and private keys to .pem files
+with open('private_key.pem','w') as pr:
+    pr.write(private_key_str)
+with open('public_key.pem','w') as pr:
+    pr.write(public_key_str)
+
+# importing keys from pem files and converting to RSA key objects
+pr_key=RSA.import_key(open('private_key.pem','r').read())
+pu_key=RSA.import_key(open('public_key.pem','r').read())
+
+# encryption part
+cipher= PKCS1_OAEP.new(key=public_key)
+cipher_text= cipher.encrypt(mssg)
+print((cipher_text))
+
+#decryption part
+decrypt_cipher=PKCS1_OAEP.new(key=private_key)
+decrypt_mssg=decrypt_cipher.decrypt(cipher_text)
+print(decrypt_mssg)
---
+++ Symm_Encryption.py
@@ -0,0 +1,49 @@
+#---------------------Symmetric Ency=ryption for strings and files---------------------------
+
+from cryptography.fernet import Fernet
+file_name='Test.txt'
+
+def write_key():
+    key=Fernet.generate_key()
+    with open('key.key','wb') as key_file:
+        key_file.write(key)
+
+def load_key():
+    file=open('key.key','rb')
+    k=file.read()
+    return k
+
+def encrypt_file(filename,key):
+    f= Fernet(key)
+    with open(filename,'rb') as file:
+        mssg=file.read()
+    encrypt_mssg= f.encrypt(mssg)
+    with open(filename,'wb') as file:
+        file.write(encrypt_mssg)
+
+def decrypt_file(filename,key):
+    f= Fernet(key)
+    with open(filename,'rb') as file:
+        encrypt_mssg= file.read()
+    decrypt_mssg= f.decrypt(encrypt_mssg)
+    with open(filename,'wb') as file:
+        file.write(decrypt_mssg)
+
+
+write_key()
+key=load_key()
+message='Message to be symmetrically encrypted'.encode()
+ # converts strings to bytes to be suitable for encryption
+ #encode() encodes that string using utf-8 codec
+f=Fernet(key)
+encrypt_mssg= f.encrypt(message)
+decrypt_mssg= f.decrypt(encrypt_mssg)
+
+
+key1=load_key()
+with open(file_name,'w') as f:
+    text="This is an implementation of proxy encryption which is a " \
+         "type of Asymmetric encryption.This is more safer than symmetric encryption"
+    f.write(text)
+encrypt_file(file_name,key1)
+decrypt_file(file_name,key1)
