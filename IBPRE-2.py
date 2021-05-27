97414f7a0c63e1441461a49e4eef706d73e9b020
Nalin Prabhath <48054155+nalin2002@users.noreply.github.com>
2021-05-27 18:28:14 +0530

    IBPRE using charm library

--- IBPRE-2.py
+++ IBPRE-2.py
@@ -3,35 +3,36 @@ from charm.core.math.integer import integer, bitsize, int2Bytes, randomBits
 from charm.toolbox.hash_module import Hash
 from charm.core.engine.util import objectToBytes
 from charm.toolbox.pairinggroup import PairingGroup,pc_element
-debug = False
+debug = True
 
 
 class PreGA:
-
+    global group,h
     def __init__(self, groupObj):
-        global group, h
-        group = groupObj
-        h = Hash(group)
+        #global group, h
+        self.group = groupObj
+        self.h = Hash(group)
 
     def setup(self):
-        s = group.random(ZR)
-        g = group.random(G1)
+        s = (self.group).random(ZR)
+        g = (self.group).random(G1)
         # choose H1-H6 hash functions
         msk = {'s': s}
         params = {'g': g, 'g_s': g ** s}
         if (debug):
             print("Public parameters...")
-            group.debug(params)
+            (self.group).debug(params)
             print("Master secret key...")
-            group.debug(msk)
+            (self.group).debug(msk)
+
         return (msk, params)
 
     def keyGen(self, msk, ID):
-        k = group.hash(ID, G1) ** msk['s']
+        k = (self.group).hash(ID, G1) ** msk['s']
         skid = {'skid': k}
         if (debug):
             print("Key for id => '%s'" % ID)
-            group.debug(skid)
+            (self.group).debug(skid)
         return skid
 
     def encrypt(self, params, ID, M):
@@ -39,28 +40,28 @@ class PreGA:
         if bitsize(enc_M) / 8 > group.messageSize():
             print("Message cannot be encoded.")
             return None
-        sigma = group.random(GT)
-        r = h.hashToZr(sigma, enc_M)
+        sigma = (self.group).random(GT)
+        r = (self.h).hashToZr(sigma, enc_M)
         A = params['g'] ** r
-        B = sigma * pair(params['g_s'], group.hash(ID, G1) ** r)
-        C = enc_M ^ h.hashToZn(sigma)
+        B = sigma * pair(params['g_s'], (self.group).hash(ID, G1) ** r)
+        C = enc_M ^ (self.h).hashToZn(sigma)
         C_ = {'A': A, 'B': B, 'C': C}
-        S = group.hash((ID, C_), G1) ** r
+        S = (self.group).hash((ID, C_), G1) ** r
         ciphertext = {'S': S, 'C': C_}
         if (debug):
             print('\nEncrypt...')
             print('r => %s' % r)
             print('sigma => %s' % sigma)
             print('enc_M => %s' % enc_M)
-            group.debug(ciphertext)
+            (self.group).debug(ciphertext)
         return ciphertext
 
     def decryptFirstLevel(self, params, skid, cid, ID):
-        H = group.hash((ID, cid['C']), G1)
-        t = group.random(ZR)
+        H = (self.group).hash((ID, cid['C']), G1)
+        t = (self.group).random(ZR)
         sigma = cid['C']['B'] / (pair(cid['C']['A'], skid['skid'] * H ** t) / pair(params['g'] ** t, cid['S']))
-        m = cid['C']['C'] ^ h.hashToZn(sigma)
-        r = h.hashToZr(sigma, m)
+        m = cid['C']['C'] ^ (self.h).hashToZn(sigma)
+        r = (self.h).hashToZr(sigma, m)
         if (cid['S'] != H ** r) or (cid['C']['A'] != params['g'] ** r):
             if debug: print("Decryption Failed")
             return None
@@ -74,21 +75,21 @@ class PreGA:
         return int2Bytes(m)
 
     def rkGen(self, params, skid, IDsrc, IDdest):
-        N = integer(randomBits(group.secparam))
-        K = pair(skid['skid'], group.hash(IDdest, G1))
+        N = integer(randomBits((self.group).secparam))
+        K = pair(skid['skid'], (self.group).hash(IDdest, G1))
         if (debug):
             print("\nRe-encryption key for id1 => '%s' to id2 => '%s'" % (IDsrc, IDdest))
-            group.debug(skid)
+            (self.group).debug(skid)
             print('N => %s' % N)
             print('K => %s' % K)
-        return {'N': N, 'R': group.hash((K, IDsrc, IDdest, N), G1) * skid['skid']}
+        return {'N': N, 'R': (self.group).hash((K, IDsrc, IDdest, N), G1) * skid['skid']}
 
     def reEncrypt(self, params, IDsrc, rk, cid):
-        H = group.hash((IDsrc, cid['C']), G1)
+        H = (self.group).hash((IDsrc, cid['C']), G1)
         if pair(params['g'], cid['S']) != pair(H, cid['C']['A']):
             if debug: print("Re-encryption Failed")
             return None
-        t = group.random(ZR)
+        t = (self.group).random(ZR)
         B_ = cid['C']['B'] / (pair(cid['C']['A'], rk['R'] * H ** t) / pair(params['g'] ** t, cid['S']))
         if (debug):
             print('\nRe-ncrypt...')
@@ -98,10 +99,10 @@ class PreGA:
         return {'A': cid['C']['A'], 'B': B_, 'C': cid['C']['C'], 'IDsrc': IDsrc, 'N': rk['N']}
 
     def decryptSecondLevel(self, params, skid, IDsrc, ID, cid):
-        K = pair(group.hash(IDsrc, G1), skid['skid'])
+        K = pair((self.group).hash(IDsrc, G1), skid['skid'])
         sigma = cid['B'] * pair(cid['A'], group.hash((K, IDsrc, ID, cid['N']), G1))
-        m = cid['C'] ^ h.hashToZn(sigma)
-        r = h.hashToZr(sigma, m)
+        m = cid['C'] ^ (self.h).hashToZn(sigma)
+        r = (self.h).hashToZr(sigma, m)
         if (cid['A'] != params['g'] ** r):
             if debug: print("Decryption second level Failed")
             return None
